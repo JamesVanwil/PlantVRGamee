8d3cc8b05d2dd5e74b06ab1881cfedcd1721e8c6
JamesVanwil <james.vanwil@gmail.com>
2023-11-02 13:25:51 +0100

    GlassAnimation

---
+++ GameThing/Assets/PlayAnimation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    public Animator glassAnimation;
+
+    public void PlayGlassAnimation()
+    {
+        glassAnimation.SetTrigger("Open");
+    }
+}
