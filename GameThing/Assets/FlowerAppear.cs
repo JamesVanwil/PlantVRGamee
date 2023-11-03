f4806c65e7d48210800f2a1b46204eed212875db
JamesVanwil <james.vanwil@gmail.com>
2023-11-03 01:15:49 +0100

    fix

--- GameThing/Assets/FlowerAppear.cs
+++ GameThing/Assets/FlowerAppear.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowerAppear: MonoBehaviour
+public class FlowerAppear : MonoBehaviour
 {
-    public GameObject cubeToDisappear;
-    public GameObject objectToAppear;
+
+    public GameObject cubeToDisappear; // Reference to the cube that will disappear
+    public GameObject cubeToAppear;    // Reference to the cube that will appear
+
+    private bool hasSwitched = false;  // Track whether the cubes have been switched
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == cubeToDisappear)
+        if (!hasSwitched)
         {
-            Debug.Log("Trigger entered by cubeToDisappear");
-            objectToAppear.SetActive(true);   // Show the new object
-            cubeToDisappear.SetActive(false); // Hide the cube
-        }
-        else
-        {
-            Debug.Log("Trigger entered by something else");
+            if (other.gameObject == cubeToDisappear)
+            {
+                cubeToDisappear.SetActive(false); // Hide the cube to disappear
+                cubeToAppear.SetActive(true);     // Show the cube to appear
+                hasSwitched = true;              // Mark the switch as complete
+            }
         }
     }
-
 }
+
+
--- GameThing/Assets/SeedFusion.cs
+++ GameThing/Assets/SeedFusion.cs
@@ -10,21 +10,34 @@ public class SeedFusion : MonoBehaviour
     public Collider zoneCollider; // Reference to the zone's collider
     public GameObject Seed;
 
+    public GameObject object1; // Reference to the first object
+    public GameObject object2; // Reference to the second object
+
+    private bool objectsCombined = false;
+
     public void Start()
     {
         Seed.SetActive(false);
     }
+
     private void Update()
     {
-        // Check if both objects are in the zone
-        if (zoneCollider.bounds.Contains(object1Collider.bounds.center) &&
-            zoneCollider.bounds.Contains(object2Collider.bounds.center))
+        if (!objectsCombined)
         {
-            // Trigger the animation
-            Seed.SetActive(true);
-            animator.SetTrigger("PlayAnimation");
-            object1Collider.enabled = false;
-            object2Collider.enabled = false;
+            // Check if both objects are in the zone
+            if (zoneCollider.bounds.Contains(object1Collider.bounds.center) &&
+                zoneCollider.bounds.Contains(object2Collider.bounds.center))
+            {
+                // Deactivate or destroy the objects
+                object1.SetActive(false); // Deactivate the first object
+                object2.SetActive(false); // Deactivate the second object
+
+                // Trigger the animation
+                Seed.SetActive(true);
+                animator.SetTrigger("PlayAnimation");
+
+                objectsCombined = true; // Mark that the objects have been combined
+            }
         }
     }
 }
